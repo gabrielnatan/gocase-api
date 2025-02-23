e5caf392a75f91615da9230f5436d750f7962002
Gabriel Natan <gabriel--natan@live.com>
2025-02-22 22:18:55 -0300

    feat: create DeleteUserUseCase

---
+++ src/application/user/delete/delete-user.use-case.ts
@@ -0,0 +1,18 @@
+import { IUseCase } from "../../../@sahred/application/use-case.interface.js";
+import { Uuid } from "../../../@sahred/domain/value-object/uuid/uuid.entity.js";
+import { IUserRepository } from "../../../domain/user/entity/repository/user.repository.interface.js";
+
+export class DeleteUserUseCase implements IUseCase<DeleteUserInput,DeleteUserOutput>{
+    constructor(private readonly userRepo:IUserRepository){}
+    async execute(input: DeleteUserInput): Promise<DeleteUserOutput> {
+        const uuid = new Uuid(input.id)
+        await this.userRepo.delete(uuid)
+        return { success: true };
+    }
+}
+
+export type DeleteUserInput = {
+    id: string
+};
+  
+export type DeleteUserOutput = { success: true };;
