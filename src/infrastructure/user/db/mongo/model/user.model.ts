2b19883736d952e17ef099004a4edbb788389b4e
Gabriel Natan <gabriel--natan@live.com>
2025-02-22 22:22:57 -0300

    feat: create IUser

---
+++ src/infrastructure/user/db/mongo/model/user.model.ts
@@ -0,0 +1,44 @@
+import mongoose, { Schema, Document, Model } from 'mongoose';
+import { v4 as uuidv4 } from 'uuid';
+
+type Role = 'admin' | 'user';
+
+export interface IUser extends Document {
+  id: string;
+  first_name: string;
+  last_name: string;
+  email: string;
+  password: string;
+  role: Role;
+  created_at: Date;
+  updated_at?: Date;
+  deleted_at?: Date;
+}
+
+const UserSchema: Schema = new Schema<IUser>({
+  id: { 
+    type: String, 
+    required: true, 
+    unique: true,
+    default: uuidv4,  // Gera automaticamente um UUID
+    validate: {
+      validator: function(v: string) {
+        return /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i.test(v);
+      },
+      message: (props) => `${props.value} não é um UUID válido!`
+    }
+  },
+  first_name: { type: String, required: true },
+  last_name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, required: true, enum: ['admin', 'user'] },
+  created_at: { type: Date, required: true, default: Date.now },
+  updated_at: { type: Date },
+  deleted_at: { type: Date },
+});
+
+export const UserModel: Model<IUser> = mongoose.model<IUser>(
+  'User',
+  UserSchema
+);
