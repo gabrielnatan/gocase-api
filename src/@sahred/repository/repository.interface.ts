dabaffc818fbd0702484caf9b610e811fd389e96
Gabriel Natan <gabriel--natan@live.com>
2025-02-22 22:18:15 -0300

    refactor: reposition code for better readability

--- src/@sahred/repository/repository.interface.ts
+++ src/@sahred/repository/repository.interface.ts
@@ -3,8 +3,8 @@ import { ValueObject } from "../domain/value-object/value-object.js"
 
 export interface IRepository<E extends Entity, EntityId extends ValueObject>{
     insert(entity:E):Promise<void>
+    update(entity:E):Promise<void>
     delete(entityId:EntityId):Promise<void>
-    update(entityId:EntityId):Promise<void>
     
     findById(entityId:EntityId):Promise<E | null>
     findAll():Promise<E[] | null>
